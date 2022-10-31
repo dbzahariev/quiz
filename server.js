13476bc8123ad6b63412760124c09c9210c03ae8
Dimitar Zahariev <dbzahariev@gmail.com>
2022-10-31 03:14:23 +0200

    Remove / api

--- server.js
+++ server.js
@@ -32,15 +32,19 @@ app.use('/api/profile', profile);
 app.use('/api/quiz', quiz);
 app.use('/api/users', users);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(publicPath, 'index.html'));
+// });
 
-app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello World!'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.send({
+//         message: 'Hello World!'
+//     });
+// });
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
 module.exports = { app };
