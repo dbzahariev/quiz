5e285be16ba33744494f761674801d78edd81ea1
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-18 21:00:47 +0200

    Fix url from railway

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import axios from 'axios';
 import './styles/styles.scss';
 import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
-import App from './App';
 import './app.scss';
-import axios from 'axios';
 
 // if (process.env.NODE_ENV !== "development") {
-  axios.defaults.baseURL = "https://http-nodejs-production-c2b5.up.railway.app";
+axios.defaults.baseURL = process.env.BASE_URL;
 // }
 
 ReactDOM.render(
