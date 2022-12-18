38e1443aff75ec6d7fd9604ab28d292e0257045c
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-18 09:24:31 +0200

    Fix build

--- client/src/App.js
+++ client/src/App.js
@@ -23,24 +23,6 @@ import GameColorcheck from "./components/games/GameColorcheck"
 import PrivateRoute from './components/common/PrivateRoute';
 
 import store from './store';
-import setAuthToken from './utils/setAuthToken';
-import { logoutUser, setCurrentUser } from './actions/authActions';
-
-if (localStorage.jwtToken) {
-	// set auth tokwn to header auth
-	setAuthToken(localStorage.jwtToken);
-	// Decode token and get user info
-	const decoded = jwt_decode(localStorage.jwtToken);
-	// Set user and authenticated
-	store.dispatch(setCurrentUser(decoded));
-	// Check for expired token
-	const currentTime = Date.now() / 1000;
-	if (decoded.exp < currentTime) {
-		// Logout out user
-		store.dispatch(logoutUser());
-		window.location.href = '/';
-	}
-}
 
 class App extends Component {
 	render() {
